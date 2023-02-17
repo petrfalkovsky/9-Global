35d64a7f5f8241337e8913f00cdaca6e00052664
Peter Terentev <petrfalkovsky@yandex.ru>
2023-02-17 16:16:58 +0300

    fix: add id back to top button to all pages

--- common.blocks/button-link/button-link__back-top/button-link__back-top.js
+++ common.blocks/button-link/button-link__back-top/button-link__back-top.js
@@ -1,6 +1,6 @@
 export function backToTop() {
     "use strict";
-    const target = document.getElementById("footer");
+    const target = document.getElementById("back-to-top-target-id");
     const link = document.getElementById("back-to-top");
     link.style.display = "none";
     window.addEventListener("scroll", function () {
